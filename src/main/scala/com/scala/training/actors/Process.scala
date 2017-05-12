31ee897817cee5de5a6f47a797c9a68442a219fe
krishnaveniN <krishnaveni.n@tvsnext.io>
2017-05-12 19:04:16 +0530

    added customer repo

--- src/main/scala/com/scala/training/actors/Process.scala
+++ src/main/scala/com/scala/training/actors/Process.scala
@@ -9,10 +9,10 @@ import com.mongodb.MongoException
 import org.bson.types.ObjectId
 import com.scala.training.domain._
 import com.scala.training.repo.{StudentRepoT, Student}
-
+import com.scala.training.repo.{CustomerRepoT, Customer}
 import scala.concurrent.ExecutionContext
 
-class ProcessActor(studentRepo:StudentRepoT)(implicit val exe:ExecutionContext) extends Actor with ActorLogging{
+class ProcessActor(studentRepo:StudentRepoT,customerRepo:CustomerRepoT)(implicit val exe:ExecutionContext) extends Actor with ActorLogging{
 
   implicit val timeout=Timeout(5000L)
 
@@ -22,8 +22,14 @@ class ProcessActor(studentRepo:StudentRepoT)(implicit val exe:ExecutionContext) 
 
   val second=Student(name = "yyy",age = 25,bloodGroup = "AB+",position = "BE")
 
+  val cust1=Customer(id=uuid ,name = "Symscribe",country="US",Domain = "Angular & Nodejs")
+  val cust2=Customer(name = "Rico",country= "UK", Domain = "Aangular & Asp.net")
+  val cust3=Customer(name = "Viridity",country="UK",Domain = "Aangular & Asp.net")
+
+
   def studentActor=context.child("student-repo").getOrElse(context.system.deadLetters)
 
+  def customerActor=context.child("customer-repo").getOrElse(context.system.deadLetters)
 
   override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 3){
     case _:MongoException=>Stop
@@ -32,9 +38,11 @@ class ProcessActor(studentRepo:StudentRepoT)(implicit val exe:ExecutionContext) 
 
   override def preStart(): Unit = {
      startStudent
+    startCustomer
   }
 
-  def startStudent=context.actorOf(Props(new RepoActor(studentRepo)),"student-repo")
+  def startStudent=context.actorOf(Props(new StudentRepoActor(studentRepo)),"student-repo")
+  def startCustomer=context.actorOf(Props(new CustomerRepoActor(customerRepo)),"customer-repo")
 
   context.system.log
    def receive ={
@@ -47,13 +55,24 @@ class ProcessActor(studentRepo:StudentRepoT)(implicit val exe:ExecutionContext) 
      case "No Match Found"=>println("No Match")
      case "student"=>startStudent
      case Results(data)=> data.foreach(println(_))
+
+     case "a"=> customerActor ! Insert(cust1)
+     case "b"=> (customerActor ? "all").mapTo[List[Customer]].foreach(println(_))
+     case "c"=> customerActor ! GetById(uuid)
+     case "d"=>customerActor ! DeleteById(uuid)
+     case "e" => customerActor ! Insert(cust2)
+     case customer:Customer=>println(s"Recv $customer")
+     case "No Match Found"=>println("No Match")
+     case "customer"=>startCustomer
+
+
      case _=>
    }
 }
 
-class RepoActor(studentRepo:StudentRepoT) extends Actor with ActorLogging{
+class StudentRepoActor(studentRepo:StudentRepoT) extends Actor with ActorLogging{
   def receive ={
-    case Insert(student)=>
+    case Insert(student:Student)=>
       studentRepo.insert(student)
       println("Inserted")
     case GetById(id) =>
@@ -71,3 +90,25 @@ class RepoActor(studentRepo:StudentRepoT) extends Actor with ActorLogging{
     case _=>
   }
 }
+
+class CustomerRepoActor(customerRepo:CustomerRepoT) extends Actor with ActorLogging{
+  def receive ={
+    case Insert(customer:Customer)=>
+      customerRepo.insert(customer)
+      println("Inserted")
+    case GetById(id) =>
+      customerRepo.get(id) match {
+        case Some(data)=> sender() ! data
+        case None=> sender() ! "No Match Found"
+      }
+      println("sent data")
+    case DeleteById(id) => customerRepo.delete(id)
+
+    case  "all"=> sender() !  customerRepo.getAll.toList
+      println("sent")
+
+
+    case _=>
+  }
+}
+
