8c21565443666f032cb879b3a928262ff59fd19f
krishnaveniN <krishnaveni.n@tvsnext.io>
2017-05-12 19:03:26 +0530

    added customer repo

--- src/main/scala/com/scala/training/Main.scala
+++ src/main/scala/com/scala/training/Main.scala
@@ -2,8 +2,9 @@ package com.scala.training
 
 import akka.actor.{ActorSystem, Props}
 import com.mongodb.casbah.{MongoClient, MongoClientURI}
-import com.scala.training.actors.{ProcessActor}
+import com.scala.training.actors.ProcessActor
 import com.scala.training.repo.MongoStudentRepo
+import com.scala.training.repo.MongoCustomerRepo
 import com.scala.training.utils.BootstrapEmbeddedMongo
 import com.typesafe.config.ConfigFactory
 
@@ -13,19 +14,21 @@ import scala.io.StdIn
 
 object Main extends App with BootstrapEmbeddedMongo{
 
-  val settings=Settings(ConfigFactory.load().getConfig("student"))
+  val settings=Settings(ConfigFactory.load().getConfig("application"))
+
 
   def DBHost=settings.dbHost
   def DBPort=settings.dbPort
   def DBName=settings.dbName
 
-  val system= ActorSystem("student",settings.conf)
+  val system= ActorSystem("twoRepo",settings.conf)
+
   startupMongo()
 
   mongoClient=MongoClient(MongoClientURI(settings.dbUrl))
-  val studentRepo=new MongoStudentRepo(mongoClient,DBName,settings.dbTable)
-  val processRepo =system.actorOf(Props(new ProcessActor(studentRepo)(system.dispatcher)),"process-actor")
-
+  val studentRepo=new MongoStudentRepo(mongoClient,DBName,"Student")
+  val customerRepo=new MongoCustomerRepo(mongoClient,DBName,"customer")
+  val processRepo =system.actorOf(Props(new ProcessActor(studentRepo,customerRepo)(system.dispatcher)),"process-actor")
   while (true){
     StdIn.readLine()  match {
       case "2"=>processRepo ! "2"
@@ -34,6 +37,13 @@ object Main extends App with BootstrapEmbeddedMongo{
       case "5"=>processRepo ! "5"
       case "6"=>processRepo ! "6"
       case "7"=> processRepo ! "student"
+
+      case "a"=>processRepo ! "a"
+      case "b"=>processRepo ! "b"
+      case "c"=>processRepo ! "c"
+      case "d"=>processRepo ! "d"
+      case "e"=>processRepo ! "e"
+      case "f"=> processRepo ! "customer"
       case _=> sys.exit(0)
     }
   }
