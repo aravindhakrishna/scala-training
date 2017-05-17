95dd9f764c7f12ff19726bb8f94d1941cbed3268
krishnaveniN <krishnaveni.n@tvsnext.io>
2017-05-17 18:40:31 +0530

    modifeied for employee

--- src/main/scala/com/scala/training/actors/WebServiceActor.scala
+++ src/main/scala/com/scala/training/actors/WebServiceActor.scala
@@ -4,27 +4,27 @@ import akka.actor.SupervisorStrategy.{Restart, Stop}
 import akka.actor.{Props, OneForOneStrategy, SupervisorStrategy, ActorRef}
 import akka.io.IO
 import com.mongodb.MongoException
-import com.scala.training.repo.{StudentRepoT, Student}
+import com.scala.training.repo.{EmployeeRepoT, Employee}
 import com.scala.training.routes.RestRoute
 import org.bson.types.ObjectId
 import spray.can.Http
 import spray.routing.HttpServiceActor
 
 
-class WebServiceActor(host:String,port:Int,studentRepoT: StudentRepoT) extends HttpServiceActor with RestRoute{
+class WebServiceActor(host:String,port:Int,employeeRepoT: EmployeeRepoT) extends HttpServiceActor with RestRoute{
 
   import context.dispatcher
 
   IO(Http)(context.system) ! Http.Bind(listener = self, interface = host, port = port)
 
-  def startStudent=context.actorOf(Props(new RepoActor(studentRepoT)),"student-repo")
+  def startEmployee=context.actorOf(Props(new RepoActor(employeeRepoT)),"emp-repo")
 
   override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 3){
     case _:Exception=>Restart
   }
 
   override def preStart() ={
-    startStudent
+    startEmployee
   }
 
  def receive = runRoute{
