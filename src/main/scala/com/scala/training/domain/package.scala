c3937321d711583ee0acf61f9902774ed47f5dc0
krishnaveniN <krishnaveni.n@tvsnext.io>
2017-05-17 18:38:51 +0530

    modifeied for employee

--- src/main/scala/com/scala/training/domain/package.scala
+++ src/main/scala/com/scala/training/domain/package.scala
@@ -1,17 +1,17 @@
 package com.scala.training
 
-import com.scala.training.repo.Student
+import com.scala.training.repo.Employee
 
 
 package object domain {
 
 
 
-  case class Insert(student: Student)
+  case class Insert(empolyee: Employee)
 
   case class GetById(id:String)
 
   case class DeleteById(id:String)
 
-  case class Results(students:List[Student])
+  case class Results(employees:List[Employee])
 }
