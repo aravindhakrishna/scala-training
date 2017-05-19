7786d36d6c67b5db3191ab268cff1a7ee8d412f7
krishnaveniN <krishnaveni.n@tvsnext.io>
2017-05-19 11:32:42 +0530

    added two repos

--- src/main/scala/com/scala/training/actors/WebServiceActor.scala
+++ src/main/scala/com/scala/training/actors/WebServiceActor.scala
@@ -1,33 +1,43 @@
 package com.scala.training.actors
 
 import akka.actor.SupervisorStrategy.{Restart, Stop}
-import akka.actor.{Props, OneForOneStrategy, SupervisorStrategy, ActorRef}
+import akka.actor.{ActorRef, OneForOneStrategy, Props, SupervisorStrategy}
 import akka.io.IO
 import com.mongodb.MongoException
-import com.scala.training.repo.{EmployeeRepoT, Employee}
-import com.scala.training.routes.RestRoute
+import com.scala.training.repo.{Bank, BankRepoT, Customer, CustomerRepoT, Employee, EmployeeRepoT}
+import com.scala.training.routes.{BankRoute, EmployeeRoute,CustomerRoute}
 import org.bson.types.ObjectId
 import spray.can.Http
 import spray.routing.HttpServiceActor
 
 
-class WebServiceActor(host:String,port:Int,employeeRepoT: EmployeeRepoT) extends HttpServiceActor with RestRoute{
+trait All_Routes extends EmployeeRoute,BankRoute,CustomerRoute
+class WebServiceActor(host: String, port: Int, employeeRepoT: EmployeeRepoT, bankRepo: BankRepoT, customerRepo: CustomerRepoT) extends HttpServiceActor with All_Routes  {
 
   import context.dispatcher
 
-  IO(Http)(context.system) ! Http.Bind(listener = self, interface = host, port = port)
+    IO(Http)(context.system) ! Http.Bind(listener = self, interface = host, port = port)
 
-  def startEmployee=context.actorOf(Props(new RepoActor(employeeRepoT)),"emp-repo")
+    def startEmployee = context.actorOf(Props(new EmployeeRepoActor(employeeRepoT)), "emp-repo")
+    def startBank = context.actorOf(Props(new BankRepoActor(bankRepoT)),"bank-repo")
+    def startCustomer = context.actorOf(Props(new CustomerRepoActor(customerRepoT)), "cust-repo")
 
-  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 3){
-    case _:Exception=>Restart
+
+
+     override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 3) {
+      case _: Exception => Restart
+    }
+
+    override def preStart() = {
+      startEmployee
+      startBank
+      startCustomer
+
+    }
+
+    def receive = runRoute {
+      import context.dispatcher
+      testRoute(context) ~ bankRoute(context) ~ custRoute(context)
+    }
   }
 
-  override def preStart() ={
-    startEmployee
-  }
-
- def receive = runRoute{
-   import  context.dispatcher
-   testRoute(context)}
-}
